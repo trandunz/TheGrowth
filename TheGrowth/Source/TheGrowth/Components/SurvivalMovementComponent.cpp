e92ad117ed1edccae7549896ab5790ca4800ae9d
trandunz <54924605+trandunz@users.noreply.github.com>
2024-05-30 15:29:00 +1200

    Started prone

--- TheGrowth/Source/TheGrowth/Components/SurvivalMovementComponent.cpp
+++ TheGrowth/Source/TheGrowth/Components/SurvivalMovementComponent.cpp
@@ -1,5 +1,8 @@
 #include "SurvivalMovementComponent.h"
 
+#include "Components/CapsuleComponent.h"
+#include "GameFramework/Character.h"
+
 void USurvivalMovementComponent::BeginPlay()
 {
 	Super::BeginPlay();
@@ -8,7 +11,8 @@ void USurvivalMovementComponent::BeginPlay()
 	RotationRate = FRotator(0.0f, 500.0f, 0.0f);
 	JumpZVelocity = 700.f;
 	AirControl = 0.35f;
-	MaxWalkSpeed = JogSpeed;
+	MaxWalkSpeed = MaxJogSpeed;
+	MaxWalkSpeedCrouched = MaxCrouchSpeed;
 	MinAnalogWalkSpeed = 20.f;
 	BrakingDecelerationWalking = 2000.f;
 	BrakingDecelerationFalling = 1500.0f;
@@ -16,10 +20,28 @@ void USurvivalMovementComponent::BeginPlay()
 
 void USurvivalMovementComponent::StartSprint()
 {
-	MaxWalkSpeed = SprintSpeed;
+	MaxWalkSpeed = MaxSprintSpeed;
 }
 
 void USurvivalMovementComponent::EndSprint()
 {
-	MaxWalkSpeed = JogSpeed;
+	MaxWalkSpeed = MaxJogSpeed;
+}
+
+void USurvivalMovementComponent::Crouch(bool bClientSimulation)
+{
+	Super::Crouch(bClientSimulation);
+}
+
+void USurvivalMovementComponent::UnCrouch(bool bClientSimulation)
+{
+	Super::UnCrouch(bClientSimulation);
+}
+
+void USurvivalMovementComponent::Prone()
+{
+}
+
+void USurvivalMovementComponent::UnProne()
+{
 }
--- TheGrowth/Source/TheGrowth/Components/SurvivalMovementComponent.h
+++ TheGrowth/Source/TheGrowth/Components/SurvivalMovementComponent.h
@@ -15,10 +15,20 @@ class THEGROWTH_API USurvivalMovementComponent : public UCharacterMovementCompon
 protected:
 	void StartSprint();
 	void EndSprint();
+	
+	void Crouch(bool bClientSimulation) override;
+	void UnCrouch(bool bClientSimulation) override;
+
+	void Prone();
+	void UnProne();
 
 protected:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category= Movement, meta = (AllowPrivateAccess = "true"))
-	float JogSpeed{500.0f};
+	float MaxJogSpeed{500.0f};
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category= Movement, meta = (AllowPrivateAccess = "true"))
-	float SprintSpeed{900.0f};
+	float MaxSprintSpeed{900.0f};
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category= Movement, meta = (AllowPrivateAccess = "true"))
+	float MaxCrouchSpeed{300.0f};
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category= Movement, meta = (AllowPrivateAccess = "true"))
+	float MaxProneSpeed{100.0f};
 };
